eadcf5a4dafd7a13a2e28bcb3e12932804ef352d
Alexander Martinez <alexmtnezf@gmail.com>
2024-12-17 07:03:05 +0000

    repo refinement

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 add_hosts() {
     local section=$1
     local ips=(${2//,/ })
-    
+
     # Check if the section exists, if not, add it
     if ! grep -q "^\[${section}\]$" "${INVENTORY_FILE}"; then
         echo "[${section}]" >> "${INVENTORY_FILE}"
@@ -12,7 +12,9 @@ add_hosts() {
 
     # Add IPs to the section if they're not already present
     for ip in "${ips[@]}"; do
-        if ! grep -q "^${ip}$" "${INVENTORY_FILE}" -A1 | grep -q "^${ip}$"; then
+        # This searches from the current section header until the next section header or EOF
+        # and checks if the IP is already present
+        if ! sed -n "/^\[${section}\]$/,/^\[.*\]$/p" "${INVENTORY_FILE}" | grep -q "^${ip}$"; then
             echo "Adding ${ip} to [${section}]"
             # Insert IPs before the next section starts or at the end of the file
             sed -i "/^\[${section}\]$/a ${ip}" "${INVENTORY_FILE}"
@@ -36,37 +38,68 @@ add_section_if_not_exists() {
     fi
 }
 
-# Function to retrieve k3s cluster config
-retrieve_k3s_cluster_config() {
-    # Validate the number of arguments passed to the function
-    if [ "$#" -ne 3 ]; then
-        echo "Usage: install_k3s_cluster <master_ip> <master_user> <context>"
+# Function to check and enable passwordless SSH
+enable_ssh_access() {
+  local host=$1
+
+  echo "Checking SSH access for $ANSIBLE_USER on $host..."
+
+  sshpass -p "$SSH_PASSWORD" ssh -o BatchMode=yes -o ConnectTimeout=5 "$ANSIBLE_USER@$host" 'exit' 2>/dev/null
+
+  if [ $? -ne 0 ]; then
+    echo "Passwordless SSH not enabled on $host. Enabling now..."
+
+    # Copy the SSH key to the remote host
+    sshpass -p "$SSH_PASSWORD" ssh-copy-id "$ANSIBLE_USER@$host"
+
+    if [ $? -ne 0 ];then
+        echo "Failed to enable passwordless SSH on $host."
         return 1
+    else
+        echo "Passwordless SSH enabled successfully on $host."
     fi
-
-    local master_ip=$1
-    local master_user=$2
-    local context=$3
-
-    # Create the .kube directory and config file if they don't exist
-    mkdir -p ~/.kube
-    touch ~/.kube/config
-
-    # Execute k3sup install command with the provided arguments
-    k3sup install \
-        --user "$master_user" \
-        --skip-install \
-        --host "$master_ip" \
-        --context "$context" \
-        --merge \
-        --local-path ~/.kube/config \
-        --ssh-key ~/.ssh/id_rsa
+  else
+    echo "Passwordless SSH already enabled on $host."
+  fi
 }
 
+# Function to check and enable passwordless sudo
+enable_sudo_access() {
+  local host=$1
+
+  echo "Checking passwordless sudo access for $ANSIBLE_USER on $host..."
+
+  ssh "$ANSIBLE_USER@$host" "echo '$SUDO_PASSWORD' | sudo -S -n true" 2>/dev/null
+
+  if [ $? -ne 0 ]; then
+    echo "Passwordless sudo not enabled on $host. Enabling now..."
+
+    local sudo_file="/etc/sudoers.d/010-$ANSIBLE_USER-nopassword"
+    ssh "$ANSIBLE_USER@$host" "echo '$SUDO_PASSWORD' | sudo -S bash -c 'echo \"$ANSIBLE_USER ALL=(ALL) NOPASSWD: ALL\" > $sudo_file'"
+
+    if [ $? -ne 0 ]; then
+        echo "Failed to enable passwordless sudo on $host."
+        return 1
+    else
+        echo "Passwordless sudo enabled successfully on $host."
+    fi
+
+  else
+    echo "Passwordless sudo already enabled on $host."
+  fi
+}
 
 # Source the .env file if it exists
 if [ -f .env ]; then
-    source .env
+    while IFS= read -r line; do
+      # Trim leading and trailing whitespace
+      line=$(echo "$line" | sed 's/^[ \t]*//;s/[ \t]*$//')
+      # Skip empty lines and lines starting with #
+      if [[ "$line" && "${line:0:1}" != "#" ]]; then
+        # Remove inline comments and export
+        export "$(echo "$line" | cut -d'#' -f1 | sed 's/^[ \t]*//;s/[ \t]*$//')"
+      fi
+    done < .env
     echo ".env file exists and sourced."
 else
     echo ".env file does not exist. Creating from example..."
@@ -90,34 +123,46 @@ else
     cd $REPO_NAME
 fi
 
+# Check if virtual environment exists
+if [ -d "venv" ]; then
+    echo "Virtual environment already exists."
+else
+    # Create a Python virtual environment
+    python3 -m venv venv
+fi
 
-echo "Initializing localhost..."
-ansible-playbook homelab.yml
+## activate your venv
+echo "Activating virtual environment..."
+source venv/bin/activate
 
-# Check if K3s is enabled
+## install requirements
+echo "Installing requirements..."
+pip install -r requirements.txt
+
+# Check and create ANSIBLE_INVENTORY_PATH directory if it doesn't exist
+echo "Checking and creating ANSIBLE_INVENTORY_PATH directory..."
+if [ ! -d "${ANSIBLE_INVENTORY_PATH}" ]; then
+    mkdir -p "${ANSIBLE_INVENTORY_PATH}"
+fi
+# Check and create ANSIBLE_INVENTORY_FILE if it doesn't exist
+echo "Checking and creating ANSIBLE_INVENTORY_FILE..."
+
+INVENTORY_FILE=$(readlink -f "${ANSIBLE_INVENTORY_PATH}/${ANSIBLE_INVENTORY_FILE}")
+
+if [ ! -f "${INVENTORY_FILE}" ]; then
+    touch "${INVENTORY_FILE}"
+fi
+# Define localhost
+# Define local section
+REQUIRED_LOCAL_SECTION="[local]\nlocalhost ansible_connection=local ansible_user=${K3S_USERNAME}"
+# Ensure the [local] section is present
+echo "Checking and creating [local] section... ${REQUIRED_LOCAL_SECTION}"
+add_section_if_not_exists "[local]" "$REQUIRED_LOCAL_SECTION" "${INVENTORY_FILE}"
+#check if k3s is enabled
 if [ "${K3S_ENABLED}" == "true" ]; then
-    echo "K3s is enabled."
-    ## Clone K3s Ansible Repo
     echo "Cloning K3s Ansible Repo..."
     git clone https://github.com/techno-tim/k3s-ansible.git  # Additional actions for configuring K3s can be added here
     echo "K3s Ansible Repo cloned."
-    # Check and create ANSIBLE_INVENTORY_PATH directory if it doesn't exist
-    echo "Checking and creating ANSIBLE_INVENTORY_PATH directory..."
-    if [ ! -d "${ANSIBLE_INVENTORY_PATH}" ]; then
-        mkdir -p "${ANSIBLE_INVENTORY_PATH}"
-    fi
-    # Check and create ANSIBLE_INVENTORY_FILE if it doesn't exist
-    echo "Checking and creating ANSIBLE_INVENTORY_FILE..."
-    INVENTORY_FILE="${ANSIBLE_INVENTORY_PATH}/${ANSIBLE_INVENTORY_FILE}"
-    if [ ! -f "${INVENTORY_FILE}" ]; then
-        touch "${INVENTORY_FILE}"
-    fi
-    # Define localhost
-    # Define local section 
-    REQUIRED_LOCAL_SECTION="[local]\nlocalhost ansible_connection=local ansible_user=${K3S_USERNAME}"
-    # Ensure the [local] section is present
-    echo "Checking and creating [local] section... ${REQUIRED_LOCAL_SECTION}"
-    add_section_if_not_exists "[local]" "$REQUIRED_LOCAL_SECTION" "${INVENTORY_FILE}"
 
     # Check if K3S_MASTERS and K3S_NODES are not empty, and then update the inventory file accordingly
     if [ -n "${K3S_MASTERS}" ]; then
@@ -126,72 +171,64 @@ if [ "${K3S_ENABLED}" == "true" ]; then
     if [ -n "${K3S_NODES}" ]; then
         add_hosts "node" "${K3S_NODES}"
     fi
-    
-    # Check if the K3s Cluster is defined 
+
+    # Check if the K3s Cluster is defined
     # Define the second section you want to ensure is present in the file
     REQUIRED_CLUSTER_SECTION="[k3s_cluster:children]\nmaster\nnode"
     # Ensure the [k3s_cluster:children] section is present
     echo "Checking and creating [k3s_cluster:children] section... ${REQUIRED_CLUSTER_SECTION}"
     add_section_if_not_exists "[k3s_cluster:children]" "$REQUIRED_CLUSTER_SECTION" "${INVENTORY_FILE}"
-
-    echo "ANSIBLE_INVENTORY_FILE created."
-    # Check and create ANSIBLE Group Vars if it doesn't exist
-    echo "Checking and creating ANSIBLE Group Vars..."
-    if [ ! -d "${ANSIBLE_INVENTORY_PATH}/group_vars" ]; then
-        mkdir -p "${ANSIBLE_INVENTORY_PATH}/group_vars"
-    fi
-    if [ ! -f "${ANSIBLE_INVENTORY_PATH}/group_vars/all.yml" ]; then
-        cp k3s-ansible/inventory/sample/group_vars/all.yml "${ANSIBLE_INVENTORY_PATH}/group_vars/all.yml" 
-    fi
-    echo "ANSIBLE Group Vars created."
-    # update Ansible User in Group Vars
-    echo "Updating Ansible User in Group Vars..."
-    sed -i "s/ansibleuser/${ANSIBLE_USER}/g" "${ANSIBLE_INVENTORY_PATH}/group_vars/all.yml"
-    sed -i "s|Your/Timezone|${SYSTEM_TZ}|g" "${ANSIBLE_INVENTORY_PATH}/group_vars/all.yml"
-    echo "master_ip: \"{{ hostvars[groups['master'][0]]['ansible_host'] | default(groups['master'][0]) }}\"" >> "${ANSIBLE_INVENTORY_PATH}/group_vars/all.yml"
-    echo "Ansible User updated in Group Vars."
-    
-    # Initialize K3s
-    # echo "Initializing K3s..."
-    # ansible-playbook -i "${ANSIBLE_INVENTORY_PATH}/${ANSIBLE_INVENTORY_FILE}" k3s-ansible/site.yml
-    # echo "K3s initialized."
-    # echo "retrieving k3s cluster config..."
-    # # Extract the first IP address from K3S_MASTERS
-    # first_master_ip=$(echo $K3S_MASTERS | cut -d',' -f1)
-    # retrieve_k3s_cluster_config "$first_master_ip" "${K3S_USERNAME}" "${K3S_CONTEXT}"
-    # echo "k3s cluster config retrieved."
-    # echo "exporting k3s cluster config..."
-    # export KUBECONFIG=~/.kube/config
-    # echo "k3s cluster config exported."
-    # kubectl config use-context "${K3S_CONTEXT}"
-    # echo "k3s cluster info:"
-    # kubectl cluster-info
-    # echo "k3s nodes:"
-    # kubectl get nodes -o wide
-
-else
-    echo "K3s is not enabled."
 fi
 
-if [ "$SYNOLOGY_ENABLED" = true ]; then
-    echo "Synology is enabled."
-    # Additional actions for configuring Synology can be added here
-else
-    echo "Synology is not enabled."
+echo "ANSIBLE_INVENTORY_FILE created."
+# Check and create ANSIBLE Group Vars if it doesn't exist
+echo "Checking and creating ANSIBLE Group Vars..."
+if [ ! -d "${ANSIBLE_INVENTORY_PATH}/group_vars" ]; then
+    mkdir -p "${ANSIBLE_INVENTORY_PATH}/group_vars"
 fi
+if [ ! -f "${ANSIBLE_INVENTORY_PATH}/group_vars/all.yml" ]; then
+    cp k3s-ansible/inventory/sample/group_vars/all.yml "${ANSIBLE_INVENTORY_PATH}/group_vars/all.yml"
+fi
+echo "ANSIBLE Group Vars created."
+# update Ansible User in Group Vars
+echo "Updating Ansible User in Group Vars..."
+sed -i "s/ansibleuser/${ANSIBLE_USER}/g" "${ANSIBLE_INVENTORY_PATH}/group_vars/all.yml"
+sed -i "s|Your/Timezone|${SYSTEM_TZ}|g" "${ANSIBLE_INVENTORY_PATH}/group_vars/all.yml"
+sed -i "s/^apiserver_endpoint:.*/apiserver_endpoint: ${K3S_VIP}/g" "${ANSIBLE_INVENTORY_PATH}/group_vars/all.yml"
+echo "Ansible User updated in Group Vars."
 
-if [ "$DOCKER_SERVER_ENABLED" = true ]; then
-    echo "Docker server is enabled."
-    # Additional actions for configuring Docker server can be added here
-    if [ -n "$DOCKER_IMAGES" ]; then
-        echo "Running Docker images: $DOCKER_IMAGES"
-        # Run Docker images listed in DOCKER_IMAGES
-        for image in $DOCKER_IMAGES; do
-            docker run -d $image
-        done
-    else
-        echo "No Docker images specified to run."
-    fi
-else
-    echo "Docker server is not enabled."
-fi
+# Prompt the user for SSH and sudo passwords
+#read -sp "Enter SSH password for $ANSIBLE_USER: " SSH_PASSWORD
+#echo
+#read -sp "Enter sudo password for $ANSIBLE_USER: " SUDO_PASSWORD
+#echo
+
+# Extract the hosts from the [master] and [node] sections
+#hosts=$(sed -n '/^\[master\]/,/^\[/p;/^\[node\]/,/^\[/p' "$ANSIBLE_INVENTORY_PATH/$ANSIBLE_INVENTORY_FILE" | grep -E '^\s*[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | awk '{print $1}')
+
+# Iterate through the extracted hosts
+#for host in $hosts; do
+#  enable_ssh_access "$host"
+#  enable_sudo_access "$host"
+#done
+
+# Setup remote user, sudo-capable, ssh keys and basic packages.
+ansible-playbook -i $INVENTORY_FILE preinstall.yml --ask-pass --ask-become-pass
+
+# Install K3s with TechnoTim's repo
+cd k3s-ansible
+ansible-galaxy collection install -r ./collections/requirements.yml
+ansible-playbook -i $INVENTORY_FILE site.yml --ask-pass --ask-become-pass
+
+
+# Post install
+cd ../
+ansible-playbook -i $INVENTORY_FILE postinstall.yml
+sleep 30
+
+kubectl get namespaces
+kubectl get nodes
+#kubectl get all --namespace argocd|grep argocd-server
+#echo "Argocd admin password "
+#kubectl get secret argocd-initial-admin-secret  --namespace argocd -o yaml|grep password| cut -d' ' -f4|base64 -d
+#echo ""
